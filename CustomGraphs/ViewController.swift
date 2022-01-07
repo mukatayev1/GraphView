73a5ca3eb73f17eb7e6e1884e985dadbb3ece6d9
mukatayev1 <mukatayev19@gmail.com>
2022-01-07 16:10:55 +0900

    Initial Commit

---
+++ CustomGraphs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomGraphs
+//
+//  Created by Aidos Mukatayev on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
